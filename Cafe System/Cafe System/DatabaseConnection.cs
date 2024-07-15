5165c542a904e8b31c885d308a48ef80bf97b2f3
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2024-07-15 17:30:10 +0530

    Add files via upload

---
+++ Cafe System/Cafe System/DatabaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe_System
+{
+    public class DatabaseConnection
+    {
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Cafe System\CafeManagementDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+    }
+}
